3f9b53dd42be277b9b0e6f84b9bc584d431b1933
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-08-19 14:21:16 +0530

    merge

--- src/modules/Dashboard/modules/ProjectManager/ProjectManager.tsx
+++ src/modules/Dashboard/modules/ProjectManager/ProjectManager.tsx
@@ -1,7 +1,6 @@
 // import React from 'react'
 
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import style from "./ProjectManager.module.css"
 
 
