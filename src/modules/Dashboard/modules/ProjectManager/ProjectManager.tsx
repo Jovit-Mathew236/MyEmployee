5699a45aa03573b236ecc1a51ae9eac3a9890d30
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-08-19 14:14:39 +0530

    projectCard

--- src/modules/Dashboard/modules/ProjectManager/ProjectManager.tsx
+++ src/modules/Dashboard/modules/ProjectManager/ProjectManager.tsx
@@ -1,9 +1,27 @@
 // import React from 'react'
 
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import style from "./ProjectManager.module.css"
+
 
 const ProjectManager = () => {
   return (
-    <div>ProjectManager</div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className={style.taskCard}>
+        <div className="my-auto text-2xl">
+          <h3>Production</h3>
+        </div>
+        <div className="flex flex-row gap-2 bottom-16 absolute text-md">
+          <h3>5 Members</h3>
+          <h3>7 TASKS</h3>
+        </div>
+        <div className="flex gap-2 w-full p-2">
+          <Button className="w-full py-2 rounded-sm bg-white text-black" variant="ghost"> x</Button>
+          <Button className="w-full py-2 rounded-sm" variant="destructive"> x</Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
